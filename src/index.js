b6f0caf0670e935eb45eed0c9fd3b4bc066b7cb6
Mike Barkmin <mbarkmin@gmail.com>
2017-08-17 17:44:46 +0200

    fixed build process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import Absolute from "./components/Absolute";
 import Button from "./components/Button";
+import ButtonDefault from "./components/ButtonDefault";
+import ButtonError from "./components/ButtonError";
+import ButtonInfo from "./components/ButtonInfo";
+import ButtonPrimary from "./components/ButtonPrimary";
+import ButtonSuccess from "./components/ButtonSuccess";
+import ButtonWarning from "./components/ButtonWarning";
+import Card from "./components/Card";
+import CardActions from "./components/CardActions";
+import CardHeader from "./components/CardHeader";
+import CardMedia from "./components/CardMedia";
+import CardText from "./components/CardText";
+import CardTitle from "./components/CardTitle";
+import Paper from "./components/Paper";
+import Relative from "./components/Relative";
+import theme from "./theme";
 
-export { Button };
+export {
+  Absolute,
+  Button,
+  ButtonDefault,
+  ButtonError,
+  ButtonInfo,
+  ButtonPrimary,
+  ButtonSuccess,
+  ButtonWarning,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardText,
+  CardTitle,
+  Paper,
+  Relative,
+  theme
+};
