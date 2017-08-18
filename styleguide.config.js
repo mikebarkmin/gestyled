424e85d661bcbe5e365f6825439c443cc9d15faf
Mike Barkmin <mbarkmin@gmail.com>
2017-08-18 12:32:07 +0200

    update component path

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,4 +1,9 @@
-const { createConfig, babel } = require("webpack-blocks");
+const { createConfig, babel } = require('webpack-blocks');
+const path = require('path');
 module.exports = {
-  webpackConfig: createConfig([babel()])
+  webpackConfig: createConfig([babel()]),
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, '.js');
+    return `import { ${name} } from 'gestyled';`;
+  }
 };
