87c0fed37bf4525372e5ac0b5774fb6d492a6101
Mike Barkmin <mbarkmin@gmail.com>
2017-08-19 12:20:00 +0200

    change themes

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,16 +1,4 @@
-import pastel from './pastel';
+import material from './material';
 import dark from './dark';
 
-const getColorFromKey = (theme, key) => {
-  if (theme != undefined) {
-    if (key in theme.colors) {
-      return theme.colors[key];
-    }
-  }
-  if (key in pastel.colors) {
-    return pastel.colors[key];
-  }
-  return key;
-};
-
-export { getColorFromKey, pastel, dark };
+export { material, dark };
---
+++ src/theme/material.js
@@ -0,0 +1,37 @@
+const pastel = {
+  breakpoints: [32, 48, 64],
+  space: [0, 6, 12, 18, 24],
+  fontSizes: [12, 14, 16, 20, 24, 34, 45, 56, 112],
+  fontWeights: [300, 400, 500, 700],
+  colors: {
+    success: '#43a047',
+    successText: '#fff',
+    error: '#F44336',
+    errorText: '#fff',
+    info: '#2196f3',
+    infoText: '#fff',
+    warning: '#ef6c00',
+    warningText: '#fff',
+    text: 'rgba(0,0,0,0.87)',
+    caption: 'rgba(0,0,0,0.54)',
+    primary: '#8e24aa',
+    primaryText: '#fff',
+    primaryLight: '#c158dc',
+    primaryDark: '#5c007a',
+    secondary: '#fcd734',
+    secondaryText: '#000',
+    secondaryLight: '#ffffeb',
+    secondaryDark: '#c5a600'
+  },
+  fontFamily: 'Roboto, sans-serif',
+  shadowLevels: [
+    'rgba(0, 0, 0, 0.12) 0 1px 3px, rgba(0,0,0,0.24) 0 1px 2px',
+    'rgba(0, 0, 0, 0.16) 0 3px 6px, rgba(0,0,0,0.24) 0 3px 6px',
+    'rgba(0, 0, 0, 0.19) 0 10px 20px, rgba(0,0,0,0.24) 0 6px 6px',
+    'rgba(0, 0, 0, 0.25) 0 14px 28px, rgba(0,0,0,0.24) 0 10px 10px',
+    'rgba(0, 0, 0, 0.30) 0 19px 38px, rgba(0,0,0,0.24) 0 15px 12px'
+  ],
+  transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms'
+};
+
+export default pastel;
