dd5f9e518ec8777b2c9f5d54e76a1dc2f43af24e
Mike Barkmin <mbarkmin@gmail.com>
2017-08-20 13:49:05 +0200

    add prop documentation

--- src/components/ImageCropped.js
+++ src/components/ImageCropped.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import withStyle from './Base';
 import Paper from './Paper';
+import PropTypes from 'prop-types';
 
 const StyledImageCropped = styled.div`
   border-radius: ${props => (props.circle ? '50%' : '')};
@@ -12,18 +13,44 @@ const StyledImageCropped = styled.div`
 `;
 
 const ImageCropped = props => {
-  let { height, width } = props;
+  let { height, width, x, y } = props;
   if (!isNaN(height)) {
     height = height + 'px';
   }
   if (!isNaN(width)) {
     width = width + 'px';
   }
+  if (!isNaN(x)) {
+    x = x + 'px';
+  }
+  if (!isNaN(y)) {
+    y = y + 'px';
+  }
   return (
     <Paper circle={props.circle} level={props.level} width={width}>
-      <StyledImageCropped {...props} height={height} width={width} />
+      <StyledImageCropped
+        {...props}
+        height={height}
+        width={width}
+        x={x}
+        y={y}
+      />
     </Paper>
   );
+};
+
+ImageCropped.propTypes = {
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  circle: PropTypes.bool,
+  src: PropTypes.string,
+  x: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  y: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+
+ImageCropped.defaultProps = {
+  x: 0,
+  y: 0
 };
 
 export default withStyle(ImageCropped);
--- src/components/ImageCropped.md
+++ src/components/ImageCropped.md
@@ -9,3 +9,11 @@ Squared Image
 ```javascript
 <ImageCropped height={150} width={150} src="https://unsplash.it/400/200/?random" />
 ```
+Squared Image but off centered
+```javascript
+<ImageCropped height={150} width={150} x={-60} y={-20} src="https://unsplash.it/400/200/?random" />
+```
+Circle Image positioned 50% and 50%
+```javascript
+<ImageCropped level={2} circle width={150} x="50%" y="50%" src="https://unsplash.it/400/200/?random" />
+```
