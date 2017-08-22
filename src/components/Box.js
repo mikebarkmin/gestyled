586b004f0c7de632a0ebabbad9afef3ce585a2f8
Mike Barkmin <mbarkmin@gmail.com>
2017-08-22 09:20:17 +0200

    add style to box

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,9 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { Box } from 'grid-styled';
 import { color } from 'styled-system';
+import withStyle from './Base';
 
 const StyledBox = styled(Box)`
     ${color}
 `;
 
-export default props => <StyledBox {...props} />;
+const MyBox = props => <StyledBox {...props} />;
+
+export default withStyle(MyBox);
