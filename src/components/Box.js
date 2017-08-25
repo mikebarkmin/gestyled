ed19734e1ba65a3b8fa41e218a11d3e0349f6d41
Mike Barkmin <mbarkmin@gmail.com>
2017-08-25 14:58:40 +0200

    fix box no bg prop warning

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box as GSBox } from 'grid-styled';
-import { color } from 'styled-system';
 import withStyle from './Base';
 
 const StyledBox = styled(GSBox)`
-    ${color}
 `;
 
-const Box = props => <StyledBox {...props} />;
+const Box = ({ bg, ...props }) => <StyledBox {...props} />;
 
 export default withStyle(Box);
