b611b5892413bbfc94430ad434823104935b4c46
Mike Barkmin <mbarkmin@gmail.com>
2017-09-08 09:34:40 +0200

    :sparkles: add flow text

---
+++ src/components/FlowText.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import Text from './Text';
+
+const FlowText = props => <Text fontSize={[1, 2, 3]} {...props} />;
+
+export default FlowText;
---
+++ src/components/FlowText.md
@@ -0,0 +1,3 @@
+```javascript
+<FlowText>Um unseren Flow Text in Aktion zu sehen musst du nur die Größe deines Browserfensters verändern.</FlowText>
+```
